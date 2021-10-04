93f50b54af10951a4f4e7107c20669ed8ce88832
Andres Pena <penchjpc@gmail.com>
2021-10-03 23:18:00 -0400

    reorganized html. JS: created 1 function to render questions and 1 function to check answers

--- script.js
+++ script.js
@@ -5,63 +5,14 @@ let intro = document.getElementById('intro');
 let quiz = document.getElementById('card-quiz');
 let startBtn = document.getElementById('start');
 let results = document.getElementById('results');
-let secondsRemaining = 10;
+let answer = '';
 
-// Start quiz
-function startQuiz() {
-    // hides start button
-    startBtn.style.visibility = 'hidden';   
-    // initializes timer
-    timer.innerText = secondsRemaining + ' seconds remaining';
-    setTime();
-    // writes first question
-    renderQuestions();
-
-}
-// Timer function
-function setTime(){
-    
-    let counter = setInterval(function(){
-        secondsRemaining --;
-        timer.innerText = secondsRemaining + ' seconds remaining';
-        if(secondsRemaining == 0){           
-            clearInterval(counter);
-            saveScore();
-        }
-    },1000);
-    
-}
-
-// Rendering questions to the card-quiz div
-function renderQuestions(){
-    let index = 0;
-    title.innerText = questions[index].question;
-    
-}
-
-// End of quiz high scores storage
-function saveScore(){
-    // Display of message
-    title.innerText = 'All Done!';
-    intro.innerText = 'Click save for saving your score or clear all to clear all score history';
-    timer.innerText = '';
-
-    // Create and Append input field to save name
-   let input = document.createElement('input');
-   input.setAttribute('placeholder','Enter Name here');
-   input.setAttribute('type','Text');
-    quiz.append(input);
-
-    // Create and Append 'Save' and 'Clear All' buttons
-
-
-}
-
+let secondsRemaining = 30;
 
 // List of questions
 let questions = [
     {
-        question : 'JavaScript is used for', 
+        question : 'JavaScript is used for:', 
         choiceA : 'Creating the text content of the webpage',
         choiceB : 'Styling the conent of the webpage',
         choiceC : 'Powering the functionality of the webpage and making it interactive',
@@ -88,12 +39,113 @@ let questions = [
         choiceC : 'Both A and B',
         correct: 'C',
     }
-]
+];
+
+// Start quiz
+function startQuiz() {
+    // hides start button
+    startBtn.style.visibility = 'hidden';   
+    // initializes timer
+    timer.innerText = secondsRemaining + ' seconds remaining';
+    setTime();
+    // writes first question
+    renderQuestions(0);
+
+}
+// Timer 
+function setTime(){
+    
+    let counter = setInterval(function(){
+        secondsRemaining --;
+        timer.innerText = secondsRemaining + ' seconds remaining';
+        if(secondsRemaining == 0){           
+            clearInterval(counter);
+            saveScore();
+        }
+    },1000);
+    
+}
+
+// Rendering questions to the card-quiz div
+function renderQuestions(index){
+        
+    title.innerText = questions[index].question;
+    intro.innerText = '';
+    
+    // creates paragraph elements as inline-block elements
+    let pA = document.createElement('p');
+    pA.style.display = 'inline-block';
+    let pB = document.createElement('p');
+    pB.style.display = 'inline-block';
+    let pC = document.createElement('p');
+    pC.style.display = 'inline-block';
+
+    // creates buttons
+    let btnA = document.createElement('button');
+    btnA.innerText = 'A';
+    let btnB = document.createElement('button');
+    btnB.innerText = 'B';
+    let btnC = document.createElement('button');
+    btnC.innerText = 'C';
+
+    // creates a line-break elements
+    let br1 = document.createElement('br');
+    let br2 = document.createElement('br');
+    // populates paragraphs
+    pA.innerText = '   ' + questions[index].choiceA;
+    pB.innerText = '   ' + questions[index].choiceB;
+    pC.innerText = '   ' + questions[index].choiceC;
+
+    // appends buttons, paragraphs and line-breaks to card-quiz
+    quiz.appendChild(btnA);
+    quiz.appendChild(pA);
+    quiz.appendChild(br1);
+    quiz.appendChild(btnB);
+    quiz.appendChild(pB);
+    quiz.appendChild(br2);
+    quiz.appendChild(btnC);
+    quiz.appendChild(pC);
+}
+
+// Check answer
+function checkAnswer(){
+    // creates h3 element
+    let h3Results = document.createElement('h3');
+    if (answer === correct){    
+    // populates h3 element
+    h3Results.innerText = 'Answer is correct!'
+    // appends h3 element to -results- div
+    results.appendChild(h3Results);
+}
+// populates h3 element
+h3Results.innerText = 'Answer is wrong :('
+// appends h3 element to -results- div
+results.appendChild(h3Results);
+
+}
 
 
-// Counter
+// Writes assessing message
 
-// Results
+
+// End of quiz high scores storage
+function saveScore(){
+    // Display of message
+    title.innerText = 'All Done!';
+    intro.innerText = 'Click save for saving your score or clear all to clear all score history';
+    timer.innerText = '';
+
+    // Create and Append input field to save name
+   let input = document.createElement('input');
+   input.setAttribute('placeholder','Enter Name here');
+   input.setAttribute('type','Text');
+    quiz.append(input);
+
+    // Create and Append 'Save' and 'Clear All' buttons
+
+
+}
+
 
 // Answer is correct
 
@@ -101,3 +153,8 @@ let questions = [
 
 // Event listener for Start Button
 startBtn.addEventListener('click' , startQuiz);
+
+// Event listener for Choices buttons that assign value to -Answer- variable
+btnA.addEventListener('click',function(){answer = 'A'});
+btnB.addEventListener('click',function(){answer = 'B'});
+btnC.addEventListener('click',function(){answer = 'C'});
